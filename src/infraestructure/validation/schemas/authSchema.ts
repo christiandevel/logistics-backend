124a8e37b95b421579d5dd63bd6380413799c6df
Christian <christianmorenoti@gmail.com>
2025-03-23 21:46:23 -0500

    feat: Add login schema for user authentication

--- src/infraestructure/validation/schemas/authSchema.ts
+++ src/infraestructure/validation/schemas/authSchema.ts
@@ -17,3 +17,14 @@ export const registerSchema = Joi.object({
 		"string.valid": "Role must be one of the following: admin, user, driver",
 	})
 });
+
+export const loginSchema = Joi.object({
+	email: Joi.string().email().required().messages({
+		"any.required": "Email is required",
+		"string.email": "Email is not valid",
+	}),
+	password: Joi.string().required().min(8).messages({
+		"any.required": "Password is required",
+		"string.min": "Password must be at least 8 characters",
+	}),
+});
