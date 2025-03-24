d8cdae3a85dd2e0c60270a94aed161a48cb92e20
Christian <christianmorenoti@gmail.com>
2025-03-24 02:00:14 -0500

    feat: Initialize shipment controller and service with PostgreSQL repository integration

--- src/infraestructure/routes/shipmentRoutes.ts
+++ src/infraestructure/routes/shipmentRoutes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import pool from "../config/database";
+import { PostgresShipmentRepository } from "../repositories/postgresShipmentRepository";
+import { ShipmentService } from "@/application/services/shipmentService";
+import { ShipmentController } from "../server/controllers/shipmentController";
 
 const shipmentRoutes = Router();
 
-shipmentRoutes.get('/', (req, res) => {
-	res.send('Hello World');
-});
+const shipmentRepository = new PostgresShipmentRepository(pool);
+const shipmentService = new ShipmentService(shipmentRepository);
+const shipmentController = new ShipmentController(shipmentService);
 
 export default shipmentRoutes;
