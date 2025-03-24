eeb174b4a6e1096675a0dbecae953e0c92a8c11b
Christian <christianmorenoti@gmail.com>
2025-03-23 21:46:40 -0500

    feat: Refactor loginUser method in AuthController to handle requests and responses with improved error management

--- src/infraestructure/server/controllers/authController.ts
+++ src/infraestructure/server/controllers/authController.ts
@@ -26,8 +26,30 @@ export class AuthController {
     }
   };
 
-  async loginUser(): Promise<void> {
-    console.log("AuthController.loginUser()");
+  loginUser = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { email, password } = req.body;
+      const result = await this.authService.loginUser(email, password);
+      
+      const response = {
+        message: "User logged in successfully",
+        user: {
+          id: result.user.getId(),
+          email: result.user.getEmail(),
+          role: result.user.getRole(),
+        },
+        status: result.status,
+        token: result.token,
+      };
+      
+      res.status(200).json(response);
+    } catch (error) {
+      if (error instanceof Error) {
+        res.status(400).json({ message: error.message });
+      } else {
+        res.status(500).json({ message: "Internal server error" });
+      }
+    }
   }
 
   async forgotPassword(): Promise<void> {
